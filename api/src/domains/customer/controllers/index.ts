61178d2f486ac6da68e7bd6bdafe6344928f1443
Arthur <arthurlm1234@gmail.com>
2023-09-08 15:06:57 -0300

    criadas rotas para Customer

---
+++ api/src/domains/customer/controllers/index.ts
@@ -0,0 +1,33 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { CustomerService } from '../service/CustomerService';
+import { loginMiddleware,
+    verifyJWT,
+    notLoggedIn } from '../../../middlewares/auth-middlewares';
+import { statusCodes } from '../../../../utils/constants/statusCodes';
+
+export const router = Router();
+
+router.post('/login', notLoggedIn, loginMiddleware);
+
+router.post('/logout',
+    verifyJWT,
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            res.clearCookie('jwt');
+            res.status(statusCodes.NO_CONTENT).end();
+        } catch (error) {
+            next(error);
+        }
+    },
+);
+
+router.post('/',
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+        await CustomerService.create(req.body);
+        res.status(statusCodes.CREATED).end();
+        } catch (error) {
+        next(error);
+        }
+    },
+);
