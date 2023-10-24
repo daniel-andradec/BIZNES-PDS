168cdf5197023dbb71341cde0895f63723e45ad2
Arthur <arthurlm1234@gmail.com>
2023-10-24 10:34:21 -0300

    arquitetura hexagonal implementada no vendor

--- api/src/domains/users/repository/UserRepository.ts
+++ api/src/domains/users/repository/UserRepository.ts
@@ -1,12 +1,13 @@
 import { UserInterface } from "../models/User";
 import { PayloadParams } from "../types/PayloadParams";
+import { CreationAttributes } from "sequelize/types";
 
 export interface UserRepository {
     encryptPassword(password: string): Promise<string>;
-    create(user: UserInterface): Promise<UserInterface>;
+    create(user: CreationAttributes<UserInterface>): Promise<UserInterface>;
     getAll(): Promise<UserInterface[]>;
     getById(id: string): Promise<UserInterface | null>;
-    update(id: string, body: UserInterface, loggedUser: PayloadParams): Promise<UserInterface>;
+    update(id: string, body: CreationAttributes<UserInterface>): Promise<UserInterface>;
     delete(id: string): Promise<void>;
     checkPassword(oldPassword: string, newPassword: string): Promise<boolean>;
     updatePassword(id: string, newPassword: string, oldPassword: string, loggedUser: PayloadParams): Promise<void>;
