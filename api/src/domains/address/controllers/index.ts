855819f397ec59c5acd65c2106f5e0027b26bed4
Arthur <arthurlm1234@gmail.com>
2023-10-24 09:32:30 -0300

    arquitetura hexagonal implementada no address

--- api/src/domains/address/controllers/index.ts
+++ api/src/domains/address/controllers/index.ts
@@ -1,31 +1,36 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { AddressService } from '../services/AddressService';
-import { verifyJWT, checkRole } from '../../../middlewares/auth-middlewares';
+import { SequelizeAddressRepository } from '../adapters/SequelizeAddressRepository';
+import { loginMiddleware, verifyJWT, checkRole, notLoggedIn } from '../../../middlewares/auth-middlewares';
 import { userRoles } from '../../users/constants/userRoles';
 import { statusCodes } from '../../../../utils/constants/statusCodes';
+import { AddressService } from '../ports/AddressService';
 
 export const router = Router();
+
+const addressRepository = new SequelizeAddressRepository();
+const addressService = new AddressService(addressRepository);
 
 router.get('/',
   verifyJWT,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const addresses = await AddressService.getAddress(req.user!);
-      res.status(statusCodes.SUCCESS).json(addresses);
+      const address = await addressService.getAddress(req.user!);
+      res.status(statusCodes.SUCCESS).json(address);
     } catch (error) {
       next(error);
     }
-  },
+  }
 );
 
 router.put('/',
   verifyJWT,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await AddressService.update(req.body, req.user!.idUser);
-      res.status(statusCodes.NO_CONTENT).end();
+      const address = await addressService.update(req.body, req.user!.idUser);
+      res.status(statusCodes.SUCCESS).json(address);
     } catch (error) {
       next(error);
     }
-  },
+  }
 );
+
